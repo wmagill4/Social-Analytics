6c33a611bfc315f50104b15b61b4d2ace5d525a7
Bill Magill <35304879+wmagill4@users.noreply.github.com>
2018-11-06 20:29:36 -0500

    first update

---
+++ config.py
@@ -0,0 +1,12 @@
+#TwitterAppAPI
+consumer_key = "FYhR0DvS6jdEeWGPTZq3aob2D"
+consumer_secret = "WbwTdvOkwg5hxuu3e8F8kbLFITS5sjKTZH9hUqackhXIRNXBpC"
+access_token = "805796552-tbtX4NW7b7afsDncw2Zfxy4pvjsKBZg2hOUiDnOz"
+access_token_secret = "QFR6bf1PU3IqR9p78NnizrY4UDNbgQ9zzcRpE3HUluvnc"
+
+#weatherApi http://api.openweathermap.org
+weather_api_key = "25bc90a1196e6f153eece0bc0b0fc9eb"
+
+#MySQL
+username = "root"
+password = "Raptor10"
